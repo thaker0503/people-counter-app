e462131feb56f9f4699589859f33e3fd7ff0b0ab
Yatharth Thaker <52533890+thaker0503@users.noreply.github.com>
2022-08-18 12:27:03 +0530

    pp

--- script.js
+++ script.js
@@ -11,28 +11,31 @@ const count = document.getElementById('count');
         }); 
 
         //Version 0.1.0
-        // save.addEventListener('click', () => {
-        //     array.push(countValue);
-        //     console.log("Using Push() ==>")
-        //     console.log(array)
-        //     previous.innerHTML += countValue + '-';
-        //     countValue = 0;
-        //     count.innerHTML = countValue;
-        // });
-
-        //Version 0.2.0
         save.addEventListener('click', () => {
-            //create a new array and push the countValue to it
-            arrFor.push(countValue);
-            //create a condition to display single digit numbers with a 0 in front of it of countValue is less than 10
-            if(countValue < 10){
-                previous.innerHTML += `<li class="list">0${countValue} - ${new Date().toLocaleString()}</li>`;
-            }else{
-                previous.innerHTML += `<li class="list">${countValue} - ${new Date().toLocaleString()}</li>`;
+            array.push(countValue);
+            console.log("Using Push() ==>")
+            console.log(array)
+            function addDash(array) {
+                for (let i = 0; i < array.length; i++) {
+                    if (i < array.length - 1) {
+                        arrFor.push(array[i] + "-");
+                    } else {
+                        arrFor.push(array[i]);
+                    }
+                }
+                return arrFor;
             }
-            //print the array in previous span with current date and time as a list
-            
             countValue = 0;
             count.innerHTML = countValue;
         });
+
+        //Version 0.2.0
+        // save.addEventListener('click', () => {
+        //     //create a new array and push the countValue to it
+        //     arrFor.push(countValue);
+        //     //create a condition to display single digit numbers with a 0 in front of it of countValue is less than 10
+        //     previous.innerHTML += `<li class="list">${countValue < 10 ? "0" : ""}${countValue} - ${new Date().toLocaleString()}</li>`;
+        //     countValue = 0;
+        //     count.innerHTML = countValue;
+        // });
         
