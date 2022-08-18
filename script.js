ae0479fa6cd05387116f2a968a419a73519a22ee
Yatharth Thaker <52533890+thaker0503@users.noreply.github.com>
2022-08-18 15:19:23 +0530

    pp

--- script.js
+++ script.js
@@ -1,3 +1,50 @@
+// window load event listener make make container display none
+window.addEventListener("load", function() {
+    document.getElementById("a2").style.display = "none";
+    document.getElementById("a1").style.display = "flex";
+} );
+
+$(document).keypress(function (e) {
+    if (e.which == 13) {
+        passwordCheck();
+    }
+});
+
+var pass1 = document.getElementById("pass1");
+var pass2 = document.getElementById("pass2");
+var pass3 = document.getElementById("pass3");
+var pass4 = document.getElementById("pass4");
+var pass5 = document.getElementById("pass5");
+var pass6 = document.getElementById("pass6");
+
+
+function passwordCheck() {
+    var final_password = pass1.value + pass2.value + pass3.value + pass4.value + pass5.value + pass6.value;
+    if (checkPassword(final_password) === true) {
+        document.getElementById("a2").style.display = "flex";
+        document.getElementById("a1").style.display = "none";
+    } 
+    else{
+        alert("Wrong Password");
+        pass1.value = "";
+        pass2.value = "";
+        pass3.value = "";
+        pass4.value = "";
+        pass5.value = "";
+        pass6.value = "";
+        pass1.focus();
+    }
+}
+
+function checkPassword(a) {
+    var password = "000000";
+    console.log(password);
+    if (a === password) {
+        return true;
+    }
+    return false;
+}
+
 const count = document.getElementById('count');
         const inc = document.getElementById('increment');
         const save = document.getElementById('save');
@@ -15,18 +62,9 @@ const count = document.getElementById('count');
             array.push(countValue);
             console.log("Using Push() ==>")
             console.log(array)
-            function addDash(array) {
-                for (let i = 0; i < array.length; i++) {
-                    if (i < array.length - 1) {
-                        arrFor.push(array[i] + "-");
-                    } else {
-                        arrFor.push(array[i]);
-                    }
-                }
-                return arrFor;
-            }
+            previous.innerHTML += countValue + ' - ';
             countValue = 0;
-            count.innerHTML = countValue;
+            count.textContent = countValue;
         });
 
         //Version 0.2.0
@@ -39,3 +77,6 @@ const count = document.getElementById('count');
         //     count.innerHTML = countValue;
         // });
         
+const password = document.getElementById('password');
+
+       
